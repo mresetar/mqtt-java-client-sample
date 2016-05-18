b8aad25724e381ca0dc34cf24701b059b1aa1ac9
Miroslav Resetar <mresetar@croz.net>
2016-05-18 13:56:41 +0200

    Initial commit

---
+++ src/main/java/net/croz/mqtt/java/client/MqttExampleClient.java
@@ -0,0 +1,80 @@
+package net.croz.mqtt.java.client;
+
+import org.eclipse.paho.client.mqttv3.*;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Mqtt example client based on example from
+ * <a>https://www.eclipse.org/paho/clients/java/.</a>
+ */
+public class MqttExampleClient {
+
+    public static void main(String[] args) {
+        // MQTT broker address
+        String broker = "tcp://192.168.99.100:1883";
+        // Use class name as client name
+        String clientId = MqttExampleClient.class.getSimpleName();
+        MqttClientPersistence persistence = new MemoryPersistence();
+
+        try {
+            MqttClient sampleClient = new MqttClient(broker, clientId, persistence);
+
+            MqttConnectOptions connOpts = new MqttConnectOptions();
+            connOpts.setCleanSession(true);
+            connOpts.setWill("mresetar/paho/lwt", "Eclipse paho client is shut down!".getBytes(), 0, false);
+
+            System.out.println("Connecting to broker: " + broker);
+            sampleClient.connect(connOpts);
+            System.out.println("Connected");
+
+            sampleClient.setCallback(new AlertCallback());
+            sampleClient.subscribe("mresetar/alertbox/+/alert");
+
+            String content = "Hello JavaCro16 from Eclipse Paho!";
+            System.out.println("Publishing message: " + content);
+            MqttMessage message = new MqttMessage(content.getBytes());
+            message.setQos(0);
+            sampleClient.publish("mresetar/paho", message);
+            System.out.println("Message published");
+
+        } catch (MqttException me) {
+            me.printStackTrace();
+        }
+    }
+
+    private static class AlertCallback implements MqttCallback {
+
+        @Override
+        public void connectionLost(Throwable cause) {
+            System.out.println("Connection is lost");
+        }
+
+        @Override
+        public void messageArrived(String topic, MqttMessage message) throws Exception {
+            System.out.println("Message on topic: " + topic + " arrived.");
+            System.out.println("Message content: " + message);
+            playMedia();
+            //mqttClient.disconnect();
+            //System.exit(0);
+        }
+
+        private void playMedia() throws IOException {
+            Process p = Runtime.getRuntime().exec("c:\\Program Files (x86)\\VideoLAN\\VLC\\vlc.exe --fullscreen " +
+                    "--start-time=68 \"d:\\CROZ\\JavaCro16\\15 Digital Starmine in Atami Japan.mp4\"");
+            BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String s;
+            while ((s = stdInput.readLine()) != null) {
+                System.out.println(s);
+            }
+        }
+
+        @Override
+        public void deliveryComplete(IMqttDeliveryToken token) {
+            System.out.println("Message delivered");
+        }
+    }
+}
